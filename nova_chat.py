fa377ae7cd02f888a939c573d9b13bc14b95b5ba
szabadnova <rolandfelfoldi@icloud.com>
2025-05-03 22:30:58 +0200

    Update nova_chat.py

--- nova_chat.py
+++ nova_chat.py
@@ -2,14 +2,16 @@ import sys
 sys.path.append(".")  # biztos, ami biztos
 
 from nova_parser import parse_input as parse_command
-from code_engine import create_file, write_code, run_file
+from code_engine import create_file, write_code, run_code
 from nova_learn import learn_function, run_learned_function
 from search_online import search_duckduckgo
 
 def chat_loop():
     print("Szabad Nova is active! Type something (or 'exit' to quit).")
+
     while True:
         user_input = input("\nYou: ")
+
         if user_input.lower() in ["exit", "quit"]:
             print("Goodbye, Roland!")
             break
@@ -23,7 +25,7 @@ def chat_loop():
             print(write_code("knowledge", param1, param2))
 
         elif action == "run_file":
-            print(run_file("knowledge", param1))
+            print(run_code("knowledge", param1))
 
         elif action == "learn":
             print(learn_function(param1, param2))
@@ -33,7 +35,7 @@ def chat_loop():
 
         elif action == "search":
             result = search_duckduckgo(param1)
-            print("\n[Search Result]")
+            print("\n[Search Result:]")
             print(result)
 
         else:
